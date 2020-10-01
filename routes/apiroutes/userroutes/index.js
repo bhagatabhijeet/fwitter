3e376e43b2b1469239f38abed9581b25cfe99d97
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-09-30 21:04:02 -0700

    created post and get routes for users

--- routes/apiroutes/userroutes/index.js
+++ routes/apiroutes/userroutes/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const connection = require('../../../config/connection');
 
-router.get('/', (_req, res) => {
-  res.send('Hello');
+router.route('/')
+.get(async (_req, res) => {
+  // res.send('Hello');
+  const query = 'SELECT * FROM users;';
+  const [rows, fields] = await connection.query(query);
+  // console.log('rows', rows);
+  console.table(rows);
+  // console.log('fields', fields);
+  res.json(rows);
+})
+.post(async (req, res) => {
+  const userInput = req.body;
+  const query = 'INSERT INTO users SET ?;';
+  const result = await connection.query(query, userInput);
+  res.json(result);
 });
 
 module.exports = router;
